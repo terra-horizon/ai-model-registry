3ac4761105219036e393cbf3d99830fb4125b85b
voskakiscite <kvoskakis@cite.gr>
2026-03-31 17:52:19 +0300

    Update routes.py

--- bootstrap/routes.py
+++ bootstrap/routes.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 from api.health import router as health_router
 from api.photos import router as photos_router
 
 
 def configure_routes(app: FastAPI) -> None:
-    app.include_router(health_router)
-    app.include_router(photos_router)
+    api_router = APIRouter(prefix="/api")
+    api_router.include_router(health_router)
+    api_router.include_router(photos_router)
+    app.include_router(api_router)
